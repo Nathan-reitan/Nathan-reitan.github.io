15bd6ec8615e9371ebbf724327fdcb3588406ca9
Nathan Reitan <nathanreitan@gmail.com>
2020-08-31 01:19:19 -0700

    Updates

---
+++ precache-manifest.d70f062233090f795fed59b083fd5ef9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6839418b4e752ce1e7b69b0b75f3d4f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dad4085beef121fe3fd",
+    "url": "/static/css/main.6e9c8068.chunk.css"
+  },
+  {
+    "revision": "1feb66bbb93b7d41a63e",
+    "url": "/static/js/2.70a7fc01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.70a7fc01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dad4085beef121fe3fd",
+    "url": "/static/js/main.d92a7a1b.chunk.js"
+  },
+  {
+    "revision": "c061df195e3c13210bd3",
+    "url": "/static/js/runtime-main.578cfe93.js"
+  },
+  {
+    "revision": "db33d1f9a468b3fc929ff32b69328c1b",
+    "url": "/static/media/0.db33d1f9.jpg"
+  },
+  {
+    "revision": "ee45de952e72a5021b04d66ff1c1787e",
+    "url": "/static/media/demosmaller.ee45de95.gif"
+  },
+  {
+    "revision": "aa069a6d11d94c3e6afcb848ea3e22ca",
+    "url": "/static/media/j.aa069a6d.jpg"
+  },
+  {
+    "revision": "d0b097289ba0680a7bed5914a6d48adf",
+    "url": "/static/media/kushol-h.d0b09728.jpg"
+  }
+]);
